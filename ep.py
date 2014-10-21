02da29bcd6281777f5ef73f759842ef0b813c044
Fernanda <fernandacamargo_24@hotmail.com>
2014-10-21 15:10:43 -0200

    Classe Batalha Atualizada

--- ep.py
+++ ep.py
@@ -4,13 +4,48 @@ import re
 
 class Batalha:
 
-	Damage = (2 * pkmn.lvl + 10)/250 * Attack/Defense * Base + 2) * Modifier;
-	Modifier = STAB * Type * Critical * other * random.uniform(0.85, 1)
+	def TypeChart():
+		arquivo = open('tabela.txt', 'r')
+		tab = []
+		i = 0
+		line = readline()
+		while(line):
+			tab[i] = line
+			i+= 1
+			tab[i] = tab[i].split(" ")
 
+		return tab
+
+
+	def CriticalHit():
+		critical = (pkmn.spd/512);
+		temp = random.uniform(0, 1)
+		if (temp <= critical):
+			return (2 * pkmn.lvl + 5)/(pkmn.lvl+5)
+		return 1
+
+	def CalculaDano(atk):
+
+		critical = CriticalHit();
+		tab = TypeChart();
+		Type = tab[atk.typ][pkmn2.typ1] * [atk.typ][pkmn2.typ2] 
+		Modifier = STAB * Type * Critical * other * random.uniform(0.85, 1)
+		Damage = (2 * pkmn.lvl + 10)/250 * pkmn.atk/pkmn2.defe * atk.pwr + 2) * Modifier;
+		
 class Pokemon:
 
 	def __init__(self):
 	
+
+		cont = 0
+		if ((temp = fileinput.readline() is not None) and cont <= 9):
+			cont+= 1
+			lista.append(temp)
+		
+		for i in range (0, len(lista)- 1):
+			pkmn.lista2[i] = leitorDeAtk()
+		lista.append = lista2	
+
 		for i in range(0, 8):
 			pkmn.nome = lista.pop(obj=lista[0])
 			pkmn.lvl = lista.pop(obj=lista[0])
@@ -40,19 +75,12 @@ class Ataque:
 
 class Leitor:
 
-	def leitorDePokemon(self):
+	# def leitorDePokemon(self):
 
-		cont = 0
-		if ((temp = fileinput.readline() is not None) and cont <= 9):
-			cont+= 1
-			lista.append(temp)
 		
-		for i in range (0, len(lista)- 1):
-			pkmn.lista2[i] = leitorDeAtk()
-		lista.append = lista2	
 
-		pkmn = Pokemon()
-
+	# 	return pkmn = Pokemon()
+	# 	pkmn = Pokemon()
 
 	def leitorDeAtk(self):
 
@@ -66,6 +94,9 @@ class Leitor:
 
 
 leitor = Leitor()
-leitor.leitorDePokemon()
+pkmn = leitor.leitorDePokemon()
+pkmn2 = leitor.leitorDePokemon()
+
+
 	    
 
