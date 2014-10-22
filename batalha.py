4b3781451dff63c12fb8c0fab9c47ed4abd3d668
Eduardo Bier <dududcbier@gmail.com>
2014-10-22 20:48:39 -0200

    - Adicionado texto com vencedor - Implementação de accuracy => método isHit() em Batalha - Consertado erro que não checava o PP de um ataque corretamente

--- batalha.py
+++ batalha.py
@@ -38,7 +38,7 @@ class Batalha:
 				for i in range(0, nAtks):
 					print("{}. {} {}/{}".format(i + 1, self.pkmn[self.turno].getAtks(i).getNome(), self.pkmn[self.turno].getAtks(i).getPpAtual(), self.pkmn[self.turno].getAtks(i).getPp()))
 				number = int(input(""))
-				if (self.pkmn[self.turno].getAtks(i - 1).ppCheck()):
+				if (self.pkmn[self.turno].getAtks(number - 1).ppCheck()):
 					return number - 1
 				print("PP insuficiente, escolha outro ataque:")
 
@@ -82,13 +82,20 @@ class Batalha:
 		Type = tab[atk.getTyp()][self.pkmn[defending].getTyp1()] * tab[atk.getTyp()][self.pkmn[defending].getTyp2()]
 		Modifier = STAB * Type * Critical * random.uniform(0.85, 1)
 		Damage = round((((2 * self.pkmn[self.turno].getLvl() + 10)/250) * self.pkmn[self.turno].getAtk() /self.pkmn[defending].getDefe()  * atk.getPwr() + 2) * Modifier, 0);
-		self.pkmn[defending].setHpAtual(self.pkmn[defending].getHpAtual() - Damage) 
-		print("{} acerta {} com {}! {} de dano ".format(self.pkmn[self.turno].getNome(), self.pkmn[defending].getNome(), atk.getNome(), Damage))
 		
+		if (self.isHit(atk)):
+			self.pkmn[defending].setHpAtual(self.pkmn[defending].getHpAtual() - Damage) 
+			print("{} acerta {} com {}! {} de dano ".format(self.pkmn[self.turno].getNome(), self.pkmn[defending].getNome(), atk.getNome(), Damage))
+		else: print("{} não acertou {} com {}!".format(self.pkmn[self.turno].getNome(), self.pkmn[defending].getNome(), atk.getNome()))
+
 		if (self.pkmn[self.turno].isStruggling()):
 			self.pkmn[self.turno].setHpAtual(self.pkmn[self.turno].getHpAtual() - Damage / 2) 
 			print("{} se machuca! {} de dano ".format(self.pkmn[self.turno].getNome(), Damage / 2))
 
+
+	def isHit(self, atk):
+		x = random.uniform(0, 1)
+		return x <= atk.getAcu() * 0.01
 
 	def isOver(self):
 		return not (self.pkmn[0].isAlive() and self.pkmn[1].isAlive())  
@@ -101,3 +108,12 @@ while (not batalha.isOver()):
 	batalha.pkmn[batalha.turno].getAtks(i).decreasePp()
 	batalha.CalculaDano(batalha.pkmn[batalha.turno].getAtks(i))
 	batalha.AlternaTurno()
+
+if (not batalha.pkmn[0].isAlive() and not batalha.pkmn[1].isAlive()):
+	print("Empate.")
+
+elif (batalha.pkmn[0].isAlive()):
+	print("{} ganhou!!".format(batalha.pkmn[0].getNome()))
+
+else: print("{} ganhou!!".format(batalha.pkmn[1].getNome()))
+
