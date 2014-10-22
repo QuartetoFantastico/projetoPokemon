e7a3752fe77884198230064fd0ce305bd79bd20d
Eduardo Bier <dududcbier@gmail.com>
2014-10-22 13:16:27 -0200

    - Adicionados getters e setters do ataque. - Métodos que acessavam atributos do ataque agora usam os getters e setters. - Variaveis do Pokemon e do Ataque agora são privadas. - Escolhe ataque apresenta uma lista dos ataques antes de pedir para o usuário digitar o número - Valor devolvido por EscolheAtaque() arrumado. - Mensagem de dano agora mostra o ataque usado.

--- ataque.py
+++ ataque.py
@@ -1,17 +1,53 @@
 class Ataque:
 
 	def __init__(self):
-		self.nome = ''
-		self.typ = -1
-		self.acu = -1
-		self.pwr = -1
-		self.pp = -1
-		self.ppAtual = -1 
+		self._nome = ''
+		self._typ = -1
+		self._acu = -1
+		self._pwr = -1
+		self._pp = -1
+		self._ppAtual = -1 
 
 	def show(self):
-		print('\tNome: {}'.format(self.nome))
-		print('\tTipo: {}'.format(self.typ))
-		print('\tAccuracy: {}'.format(self.acu))
-		print('\tPower: {}'.format(self.pwr))
-		print('\tPP: {}/{}'.format(self.ppAtual,self.pp))
+		print('\tNome: {}'.format(self._nome))
+		print('\tTipo: {}'.format(self._typ))
+		print('\tAccuracy: {}'.format(self._acu))
+		print('\tPower: {}'.format(self._pwr))
+		print('\tPP: {}/{}'.format(self._ppAtual,self._pp))
 		print()
+
+	def setNome(self, nome):
+		self._nome = nome
+
+	def setTyp(self, typ):
+		self._typ = typ
+
+	def setAcu(self, acu):
+		self._acu = acu
+
+	def setPwr(self, pwr):
+		self._pwr = pwr
+
+	def setPp(self, pp):
+		self._pp = pp
+	
+	def setPpAtual(self, ppAtual):
+		self._ppAtual = ppAtual
+
+	def getNome(self):
+		return self._nome
+
+	def getTyp(self):
+		return self._typ
+
+	def getAcu(self):
+		return self._acu
+
+	def getPwr(self):
+		return self._pwr
+
+	def getPp(self):
+		return self._pp
+
+	def getPpAtual(self):
+		return self._ppAtual
