e6285d68c27d7b6f7fbc9cc2a3da6a880c7eef44
Eduardo Bier <dududcbier@gmail.com>
2014-10-25 14:01:45 -0200

    testIniciaTurno()

--- test.py
+++ test.py
@@ -10,43 +10,7 @@ import unittest
     # Se cada coisa está no seu lugar (ou mais ou menos isso).
     #Se falta alguma informação. Se ele devolve erro para entradas erradas.
     #
-    #
-    #<NOME>
-    #<LVL>
-    #<HP>
-    #<ATK>
-    #<DEF>
-    #<SPD>
-    #<SPC>
-    #<TYP1>
-    #<TYP2>
-    #<NUM ATKS>
-    #<NOME>
-    #<TYP>
-    #<ACU>
-    #<PWR>
-    #<PP>
-    # def setUp(self):
-    #     self.seq = range(10)
 
-    # def test_shuffle(self):
-    #     # make sure the shuffled sequence does not lose any elements
-    #     random.shuffle(self.seq)
-    #     self.seq.sort()
-    #     self.assertEqual(self.seq, range(10))
-
-    #     # should raise an exception for an immutable sequence
-    #     self.assertRaises(TypeError, random.shuffle, (1,2,3))
-
-    # def test_choice(self):
-    #     element = random.choice(self.seq)
-    #     self.assertTrue(element in self.seq)
-
-    # def test_sample(self):
-    #     with self.assertRaises(ValueError):
-    #         random.sample(self.seq, 20)
-    #     for element in random.sample(self.seq, 5):
-    #         self.assertTrue(element in self.seq)
 
 class TestPokemon(unittest.TestCase):
     
@@ -202,7 +166,7 @@ class TestPokemon(unittest.TestCase):
         r = self.pkmn.getLvl()
         self.assertEqual(r, lvl)
 
-
+#Obs: 15 pokemons necessários para testar ^
 
 class TestAtaque(unittest.TestCase):
 
@@ -294,8 +258,28 @@ class TestAtaque(unittest.TestCase):
         self.assertEqual(r, False)
 
 
-# class TestBatalha(unittest.TestCase):
+class TestBatalha(unittest.TestCase):
   
+    def setUp(self):
+        self.batalha = Batalha()
+
+    def testIniciaTurno(self):
+        x = random.randint(1,254)
+        self.pkmn[0].setSpd(x)
+        self.pkmn[1].setSpd(x + 1)
+        r = self.batalha.IniciaTurno()
+        self.assertEqual(r, 1)
+
+        self.pkmn[1].setSpd(x - 1)
+        r = self.batalha.IniciaTurno()
+        self.assertEqual(r, 0)
+
+        self.pkmn[1].setSpd(x)
+        r = self.batalha.IniciaTurno()
+        self.assertGreaterEqual(r, 0)
+        self.assertLessEqual(r, 1)
+
+#Obs: 2 pokemons necessários para testar ^
 
 if __name__ == '__main__':
     unittest.main()
