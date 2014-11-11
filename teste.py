c2cff253af05fdc7c704a4d74aa758b01fb9a1be
Eduardo Bier <dududcbier@gmail.com>
2014-11-11 19:29:21 -0200

    Criado método que a partir de um battle_state com um pokemon, le outro pokemon (da entrada padrão) e adiciona ao battle_state. O método devolve o battle_state, agora com dois pokemons.

--- teste.py
+++ teste.py
@@ -1,21 +1,80 @@
 from flask import Flask
 from flask import request
 import xml.etree.ElementTree as ET
+import pokemon
+
 app = Flask(__name__)
 
 @app.route('/battle/', methods = ['GET' ,'POST'])
-def hello_world():
+def iniciaBatalha():
 	if (request.method == 'GET'):
 		return 'Hello World!'
 	else:
-		tree = ET.parse('battle_state.xml')
-		root = tree.getroot()
-		hp = root[0].find('name').text
+		battle_state = criaBattleState('battle_state.xml')
+
+		root = ET.fromstring(battle_state)
+		hp = root[1].find('name').text
 		print(hp)
-		root[0].find('name').text = 'Raichu'
+		root[1].find('name').text = 'Raichu'
 		res = ET.tostring(root)
 
 		return res
+
+def criaBattleState(battle_state):
+
+	poke = pokemon.Pokemon()
+	tree = ET.parse(battle_state)
+	root = tree.getroot()
+
+	pkmn = ET.SubElement(root, 'pokemon')
+
+	aux = ET.SubElement(pkmn, 'name')
+	aux.text = str(poke.getNome())
+
+	aux = ET.SubElement(pkmn, 'level')
+	aux.text = str(poke.getLvl())
+
+	attributes = ET.SubElement(pkmn, 'attributes')
+	aux = ET.SubElement(attributes, 'health')
+	aux.text = str(poke.getHp())
+	aux = ET.SubElement(attributes, 'attack')
+	aux.text = str(poke.getAtk())
+	aux = ET.SubElement(attributes, 'defense')
+	aux.text = str(poke.getDefe())
+	aux = ET.SubElement(attributes, 'speed')
+	aux.text = str(poke.getSpd())
+	aux = ET.SubElement(attributes, 'special')
+	aux.text = str(poke.getSpc())
+
+	aux = ET.SubElement(pkmn, 'type')
+	aux.text = str(poke.getTyp1())
+
+	aux = ET.SubElement(pkmn, 'type')
+	aux.text = str(poke.getTyp2())
+
+	for i in range(0, poke.getNatks()):
+		atk = ET.SubElement(pkmn, 'attacks')
+
+		aux = ET.SubElement(atk, 'id')
+		x = i
+		aux.text = str(x)
+
+		aux = ET.SubElement(atk, 'name')
+		aux.text = str(poke.getAtks(i).getNome())
+
+		aux = ET.SubElement(atk, 'type')
+		aux.text = str(poke.getAtks(i).getTyp())
+
+		aux = ET.SubElement(atk, 'power')
+		aux.text = str(poke.getAtks(i).getPwr())
+
+		aux = ET.SubElement(atk, 'accuracy')
+		aux.text = str(poke.getAtks(i).getAcu())
+
+		aux = ET.SubElement(atk, 'power_points')
+		aux.text = str(poke.getAtks(i).getPp())
+
+	return ET.tostring(root)
 
 
 @app.route('/battle/attack/<id>')
