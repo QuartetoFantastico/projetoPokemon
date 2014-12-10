de1e09577aa45443e53c0fc22a5dcb193587b38b
Fernanda <fernanda.magano@usp.br>
2014-12-10 12:13:32 -0200

    -Tentativa de criar arquivo com variável global para ser usada pela IA; -Atualização do diagrama de classe e do relatório

---
+++ globals.py
@@ -0,0 +1,3 @@
+def init(valor):
+    global smart
+    smart = valor
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import batalha
 import sys
 import cliente
 import server
+import globals
 
 if (len(sys.argv) > 1):
+	globals.init(1)
 	if (sys.argv[1] == '-c' or sys.argv[1] == '-C'):
 		c = cliente.Cliente(True)
 		c.sendShutdownSignal()
@@ -14,22 +16,24 @@ if (len(sys.argv) > 1):
 
 else:
 
+	intelig = 0
 	batalha = batalha.Batalha()
-	smart = 0
-
+	
 	for i in range(0, len(sys.argv)):
 		if (sys.argv[i] == '-s'): batalha.display.toggle()
 		if (sys.argv[i] == '-v'): batalha.display.toggleVerbose()
-		if (sys.argv[i] == '-i'): smart = 1
+		if (sys.argv[i] == '-i'): intelig = 1
+
+	globals.init(intelig) 	
 
 	batalha.display.showPokemon(batalha.pkmn[0])
 	batalha.display.showPokemon(batalha.pkmn[1])
 
 	while (not batalha.isOver()):
-		if (smart):
-			i = batalha.EscolheAtaqueInteligente()
-		else:
-			i = batalha.EscolheAtaque()
+		# if (smart):
+		# i = batalha.EscolheAtaqueInteligente()
+		# else:
+		i = batalha.EscolheAtaque()
 		batalha.pkmn[batalha.turno].getAtks(i).decreasePp()
 		batalha.CalculaDano(batalha.pkmn[batalha.turno].getAtks(i))
 		batalha.showStatus()
